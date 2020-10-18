63b8de695d27a94ac954f5afef2f7469595499bc
unknown <tongjac@yahoo.com>
2020-10-17 17:49:05 -0700

    Tweaked format for button display in index.html. Split Password generation steps into separate functions for easier troubleshooting and module handling.

--- generator.js
+++ generator.js
@@ -7,36 +7,61 @@ var emoji = ["😊","👍","🙌","😍","😒","❤","🤣","💕"];
 
 // User
 var passwordLength = "";
+var isLower = false;
+var isUpper = false;
+var isNum = false;
+var isSpec = false;
+var isEmoji = false;
 var passInclude = [];
+
+
+// 
+function passLengthConfirm(){
+    passwordLength = parseInt(prompt("How long do you want your password to be? (8-128 characters)"));
+    // Debug check to see what user input is interpretted as.
+    console.log(`Parsed entered password length is: $ {passwordLength} .`);
+
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        // If they enter strings, strange characters, or numbers out of the range, code won't proceed and alert asks user to try again.
+       alert("Invalid character count. Try again please.")
+   } 
+   else {
+   confirmation();
+   generation();
+}
 
 // Function for confirming what types of characters the user wants
 function confirmation(){  
-    var isLower = confirm("Do you want lowercase letters?");
-    var isUpper = confirm("Do you want uppercase letters?");
-    var isNum = confirm("Do you want numbers?");
-    var isSpec = confirm("Do you want special characters?");
-    var isEmoji = confirm("Do you want emojis?");
+    // Resets passInclude's array in case users want a different customized password.
+    passInclude = [];
+    isLower = confirm("Do you want lowercase letters?");
+    isUpper = confirm("Do you want uppercase letters?");
+    isNum = confirm("Do you want numbers?");
+    isSpec = confirm("Do you want special characters?");
+    isEmoji = confirm("Do you want emojis?");
+    if (isLower) {passInclude.push(lowerChar);}
+    if (isUpper) {passInclude.push(upperChar);}
+    if (isNum) {passInclude.push(numChar);}
+    if (isSpec) {passInclude.push(specialChar);}
+    if (isEmoji) {passInclude.push(emoji);}
 }
 
 //Function for actually generating the password
-function genPass(){
-    passwordLength = parseInt(prompt("How long do you want your password to be? (8-128 characters)"));
-    if (passwordLength === NaN || passwordLength < 8 || passwordLength > 128) {
-       alert("Try again.")
-       passwordLength = prompt("How long do you want your password to be? (8-128 characters)");
-   } else 
-   for (let i = 0; i < passwordLength; i++) {
+function generation(){
+    for (let i = 0; i < passwordLength; i++) {
+        
+    }
+}
+
 }
 
 // What happens when you click the button
 document.querySelector("#generate-password-button").addEventListener("click", function(event) {
-    console.log("Line 38 Hi");
     event.preventDefault(); 
-    confirmation();
-    genPass();
-    }
+    passLengthConfirm();
+});
 
-)};
+
 
 
 
